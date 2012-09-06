17cea47dc1184658e3b310dc31c296b458e2ad90
Joel Hughes <rudenoise@gmail.com>
2012-09-06 14:05:59 +0100

    tidy tokens_test

--- count/tokens_test.go
+++ count/tokens_test.go
@@ -5,14 +5,21 @@ import (
 )
 
 func TestTokens(t *testing.T) {
+	var expected int
+	expected = 1
 	tm := make(TokensMap)
 	tm.Add("one")
-	if tm["one"] != 1 {
-		t.Errorf("A failing test")
+	if tm["one"] != expected {
+		t.Errorf("Expected: %d, got: %d", expected, tm["one"])
 	}
+	expected = 2
 	tm.Add("a.test")
 	tm.Add("a.test")
-	if tm["a.test"] != 2 {
-		t.Errorf("Expected 2, got %d", tm["a.test"])
+	if tm["a.test"] != expected {
+		t.Errorf("Expected %d, got %d", expected, tm["a.test"])
+	}
+	expected = 0
+	if tm["nonExistant"] != expected {
+		t.Errorf("Expected %d, got %d", expected, tm["nonExistant"])
 	}
 }
