77996955d1e66d793a8c0a3bad84b3492e936388
Joel Hughes <rudenoise@gmail.com>
2012-09-10 12:52:01 +0100

    working example token counter

--- examples/tokensInFiles.go
+++ examples/tokensInFiles.go
@@ -3,13 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"path/filepath"
-	"github.com/rudenoise/counting/dir"
-	"runtime"
 	"github.com/rudenoise/counting/count"
-	/*
+	"github.com/rudenoise/counting/dir"
+	"path/filepath"
+	"runtime"
 	"sort"
-	*/
 )
 
 var exclude = flag.String("exclude", "^$", "regexp pattern to exclude in file path")
@@ -21,29 +19,28 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	flag.Parse()
 	dirStr := filepath.Dir(flag.Arg(0))
-	fmt.Println(*exclude, *include, *lmt, *tokenRegExp, dirStr)
+
 	filePaths, err := dir.AllPaths(dirStr, *exclude, *include)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(filePaths)
+
 	limit := 5000
 	length := len(filePaths)
 
-	fmt.Println(limit, length)
 	if length < limit {
 		tMap := make(count.TokensMap)
 		count.TokensInFiles(filePaths, *tokenRegExp, tMap)
-		/*
+
 		tSlice := tMap.ToSlice()
-		//sort.Sort(count.TokenSliceByCountDesc{tSlice})
+		sort.Sort(count.TokenSliceByCountDesc{tSlice})
 		if *lmt != 0 && *lmt <= length {
 			length = *lmt
 		}
 		for i := 0; i < len(tSlice); i++ {
 			fmt.Printf("%7d\t%s\n", tSlice[i].Count, tSlice[i].Token)
 		}
-		*/
+
 	} else {
 		fmt.Printf("\nDidn't bother, you tried to meaure %d files, limit set to %d\n\n", length, limit)
 	}
