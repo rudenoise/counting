2076c51e56776be1760d54e8636c6cb447bd17dc
Rudenoise <rudenoise@gmail.com>
2012-09-14 09:56:31 +0100

    started time series file counter with git

---
+++ examples/timeSeries.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"fmt"
+	"os/exec"
+)
+
+type dataPoint struct {
+	fileName string
+	data []int
+}
+
+type series []dataPoint
+
+func main() {
+	/*
+	a := make([]int, 10)
+	fmt.Println("YO", a)
+	*/
+	// loop over the previous 5 commits via git
+	for i := 5; i > 0; i-- {
+		arg := fmt.Sprintf("master~%d", i)
+		out, err := exec.Command("git", "checkout", arg).Output()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s", out)
+	}
+	// reset repo to master
+	out, err := exec.Command("git", "checkout", "master").Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", out)
+}
